01bead065d3252e80219fd0c5b197af0d592eaf6
parry <parry@parryc.com>
2022-02-18 15:35:57 -0600

    Add RSS feed for testing

--- mod_zmnebi/controllers.py
+++ mod_zmnebi/controllers.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template, send_from_directory, current_app
+from datetime import datetime, timezone, timedelta
+
+from flask import (
+    Blueprint,
+    render_template,
+    send_from_directory,
+    current_app,
+    make_response,
+)
 from git import Repo
 from bracket_table.bracket_table import BracketTable
 from app import testing_site
+from feedgen.feed import FeedGenerator
 import os
 import markdown
 import codecs
@@ -65,6 +74,41 @@ def index():
         title="recent updates",
         t="Georgian Verbs | ქართული ზმნები",
     )
+
+
+@mod_zmnebi.route("/rss", methods=["GET"], host=_host)
+def rss():
+    _url = "https://zmnebi.com"
+    _author = "Parry"
+    _email = "parry@parryc.com"
+    fg = FeedGenerator()
+    fg.title("Georgian Verbs | zmnebi.com")
+    fg.description("All about Georgian verbs and how to use them")
+    fg.link(href=_url)
+    updates = [
+        {
+            "title": "Unprefixed AOR form added",
+            "description": "Added information on how to use the unprefixed AOR form.",
+            "link": f"{_url}#how-to-use-aor",
+            "date": datetime(
+                2022, 2, 18, 3, 0, 0, tzinfo=timezone(-timedelta(hours=6))
+            ),
+        }
+    ]
+
+    for update in updates:
+        fe = fg.add_entry()
+        fe.title(update["title"])
+        fe.link(href=update["link"])
+        fe.description(update["description"])
+        fe.guid(update["link"], permalink=True)
+        fe.author(name=_author, email=_email)
+        fe.pubDate(update["date"])
+
+    response = make_response(fg.rss_str())
+    response.headers.set("Content-Type", "application/rss+xml")
+
+    return response
 
 
 def get_html(page, host):
